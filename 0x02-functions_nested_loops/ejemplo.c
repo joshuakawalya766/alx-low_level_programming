05ca06a4734d7d380dd9c1b22988441a416072ab
userfn <joshuakawalya766@gmail.com>
2022-09-13 20:56:49 +0300

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/ejemplo.c
+++ 0x02-functions_nested_loops/ejemplo.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "holberton.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -7,7 +8,15 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+        char word[9] = "Holberton";
+        int i;
+
+	for (i = 0; i < 9; i++)
+                _putchar(word[i]);
+        _putchar('\n');
+
+        return (0);
 }
